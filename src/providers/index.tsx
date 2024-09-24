c580d956ca79eda6987b7e8c8dd01fcc18e2c789
Caio Ventura <37090076+caiovn@users.noreply.github.com>
2024-09-24 16:08:16 -0300

    fix: pr fixes

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
 import { ThemeProvider } from "./theme.provider";
 
-export function Providers({ children }) {
+export function Providers({ children }: any) {
   return <ThemeProvider>{children}</ThemeProvider>;
 }
